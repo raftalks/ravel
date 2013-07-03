6c602a5fec1cf8339e2458db7f495bd6bc253837
Victor Geere <victorgeere.public@digitalpro.co.za>
2013-07-03 16:36:59 +0200

    fix sidebar links

--- src/views/layouts/admin/partials/sidebar.blade.php
+++ src/views/layouts/admin/partials/sidebar.blade.php
@@ -10,7 +10,7 @@
 	<div id="profile-links">
 		Hello, <a href="#" title="Edit your profile"><?php echo ucfirst(currentUserName());?></a><br />
 		<br />
-		<a href="#" title="View the Site">View the Site</a> | <a href="<?php echo URL::route('ravellogout');?>" title="Sign Out">Sign Out</a>
+		<a href="/" title="View the Site">View the Site</a> | <a href="<?php echo URL::route('ravellogout');?>" title="Sign Out">Sign Out</a>
 	
 		
 	</div> 
